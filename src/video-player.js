ec53eec7a8b0cf4d3c3c840a645b5e5e901639ed
Gleb Glushtsov <gleb.glushtsov@gmail.com>
2024-02-28 15:26:03 -0800

    added playlist and streams validation

--- src/video-player.js
+++ src/video-player.js
@@ -2,19 +2,47 @@ class VideoPlayer {
     #listElement;
     #videoElement;
 
-    constructor() {
-        console.log('VideoPlayer.constructor()', arguments)
+    constructor(videoEl, listEl) {
+        this.#listElement = listEl;
+        this.#videoElement = videoEl;
     }
 
-    init(videoElement, listElement) {
-        this.#listElement = listElement;
-        this.#videoElement = videoElement;
-        console.log('VidoePlqyer.init()', arguments)
+    init(apiUrl) {
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(({data}) => this.#validateStreams(data))
+            .then(data => this.#renderPlaylist(data));
     }
 
-    test() {
-        console.log('VidoePlqyer.test()', arguments)
-        fetch('http://localhost:8000/streams', { mode: 'no-cors' }).then(response => response.json()).then(console.log);
+    #validateStreams(data) {
+        return Promise.all(data.map(stream => {
+            const {title, url} = stream;
+
+            return fetch(url, {method: 'HEAD'}).then(response => {
+                return {available: response.ok, title, url};
+            }).catch(error => {
+                console.error(error);
+                return {available: false, title, url, error: error.message};
+            });
+        }));
+    }
+
+    #renderPlaylist(data) {
+        data.forEach(stream => {
+            const {available, title, url} = stream;
+            const newOption = document.createElement('option');
+
+            newOption.value = url;
+            newOption.title = url;
+            newOption.textContent = title ?? url;
+            newOption.setAttribute('style', available ? 'color: black' : 'color: red');
+            newOption.ondblclick = () => this.#loadStream(url);
+            this.#listElement.appendChild(newOption)
+        });
+    }
+
+    #loadStream(streamUrl) {
+        this.#videoElement.setAttribute('src', streamUrl);
     }
 }
 
