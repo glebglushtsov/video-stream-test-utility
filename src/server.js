f9f9fce67e1f4be3702bae8420a7b2850ce36d52
Gleb Glushtsov <gleb.glushtsov@gmail.com>
2024-02-28 15:26:47 -0800

    added cors headers to server

--- src/server.js
+++ src/server.js
@@ -2,29 +2,31 @@ const http = require('http');
 const sqlite3 = require("sqlite3").verbose();
 
 const port = process.argv[2] ? parseInt(process.argv[2], 10) : 8080;
-
+const server = http.createServer();
 const db = new sqlite3.Database("playlist.db");
 
 function requestHandler(request, response) {
     if (request.url === '/streams') {
-        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.writeHead(200, {
+            'Content-Type':                'application/json',
+            'Access-Control-Allow-Origin': 'http://localhost:3000'
+        });
 
         db.all("SELECT * FROM streams", (err, rows) => {
             if (err) {
                 console.error(err.message);
 
-                response.end(JSON.stringify({ error: 'Internal Server Error' }));
+                response.end(JSON.stringify({error: 'Internal Server Error'}));
 
                 return;
             }
 
-            response.end(JSON.stringify(rows));
+            response.end(JSON.stringify({data: rows, error: false}));
         });
     }
 }
 
-const server = http.createServer(requestHandler)
-
+server.on('request', requestHandler);
 server.listen(port, (err) => {
     if (err) {
         return console.error(err)
